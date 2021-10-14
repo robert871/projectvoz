cb272d8581c0792679eaaae53793787db9e7bf8c
Roberto Lopes da Costa <71911175+robert871@users.noreply.github.com>
2021-10-13 23:12:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ 
+ function abrir(params) { 
+window.document.getElementById(`list`).classList.replace('list', 'list2');  
+window.document.getElementById(`close`).classList.replace('hamb2', 'hamb'); 
+
+}
+function fechar(params) { 
+    window.document.getElementById(`list`).classList.replace('list2', 'list');  
+    window.document.getElementById(`close`).classList.replace('hamb', 'hamb2'); 
+    
+    }
+
